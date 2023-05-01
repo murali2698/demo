2c977b46ae8707ab1d779d2d1f7a310e70f0c6ed
Muralidharan K <mdharan667@gmail.com>
2023-05-01 15:05:39 +0530

    sonar

--- src/main/java/com/example/demo/controller/ClassController.java
+++ src/main/java/com/example/demo/controller/ClassController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ClassController {
 
     @GetMapping("/")
-    public String getHello(){
-        System.out.println("Hello world");
+    public String getHello() {
+        log.info("Hello world");
         return "Hello world... this is maven learning the world";
     }
 }
